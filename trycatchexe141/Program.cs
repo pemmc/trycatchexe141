2e1564e0e1a94734fa5ebf4ff56fa6bd4ba1bd79
Paulo Eduardo Magaldi <pauloeduardomagaldi@hotmail.com>
2020-10-17 12:45:27 -0300

    Funcionando ...

--- trycatchexe141/Program.cs
+++ trycatchexe141/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace trycatchexe141
 {
@@ -6,9 +7,45 @@ namespace trycatchexe141
     {
         static void Main(string[] args)
         {
+            //CultureInfo culture = new CultureInfo("pt-BR");
+            
+            try
+            {
+                int n1 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Hello World!");
+                int n2 = int.Parse(Console.ReadLine());
 
+                int result = n1 / n2;
+
+                Console.WriteLine(result);
+
+            }
+          
+            //Vamos tratar/capturar UMA EVENTUAL EXCEÇÃO
+            /* TIPO GENERICO
+            catch (Exception)
+            {
+
+                Console.WriteLine("Minha mensagem em portugues e livre");
+
+            }
+            */
+
+            //TRATAMENTO ESPECÍFICO
+            catch (DivideByZeroException)
+            {
+
+              
+                Console.WriteLine("MINHA MENSAGEM DE ERRO");
+
+
+            }
+
+            catch (FormatException e)
+            {
+
+                Console.WriteLine("Entrada de dados no formatao errado: " + e.Message);
+            }
 
         }
     }
